077d68511446701e710008bdad96f664b7ae4350
aryansaud-80 <aryansaud080+github@gmail.com>
2024-06-26 20:54:21 +0545

    dynamic pokemon detail card

--- details.js
+++ details.js
@@ -1,6 +1,15 @@
 const pokemonId = new URLSearchParams(window.location.search).get('id');
 
 const introEl = document.querySelector('.intro');
+const h1El = document.querySelectorAll('h1');
+const statName = document.querySelectorAll('.stat-name');
+const hpValue = document.querySelector('.stat-value-hp');
+const atkValue = document.querySelector('.stat-value-atk');
+const defValue = document.querySelector('.stat-value-def');
+const satkValue = document.querySelector('.stat-value-satk');
+const sdefValue = document.querySelector('.stat-value-sdef');
+const spdValue = document.querySelector('.stat-value-spd');
+const progressBar = document.querySelectorAll('.progress-bar');
 
 const typeColors = {
   normal: '#A8A77A',
@@ -50,9 +59,12 @@ const displayPokemonDetails = (data)=>{
   const moves = data.moves;
   let count =0;
   introEl.style.backgroundColor = generateTypeColor(data.types[0].type.name);
+  
+
   document.querySelector('.name').innerText = data.name;
   document.querySelector('.id').innerText = `#${pokemonId.padStart(3, '0')}`;
   document.querySelector('.pokeImg').src= `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+
   
 
   types.forEach((el)=>{
@@ -62,8 +74,9 @@ const displayPokemonDetails = (data)=>{
     document.querySelector('.type-data').insertAdjacentElement('beforeend', pEl)
   })
 
-  document.querySelector('.weight').insertAdjacentText('beforeend', `${data.weight/10}kg`);
-  document.querySelector('.height').insertAdjacentText('beforeend', `${data.height/10}m`);
+
+    document.querySelector('.weight').insertAdjacentText('beforeend', `${data.weight/10}kg`);
+    document.querySelector('.height').insertAdjacentText('beforeend', `${data.height/10}m`);
 
 
   moves.forEach((el)=>{
@@ -74,6 +87,27 @@ const displayPokemonDetails = (data)=>{
     pEl.innerText = el.move.name;
     document.querySelector('.moves').insertAdjacentElement('afterbegin', pEl);
   })
+
+  statName.forEach(el=>el.style.color = generateTypeColor(data.types[0].type.name));
+
+  h1El.forEach(el=>el.style.color = generateTypeColor(data.types[0].type.name));
+  hpValue.innerText = data.stats[0].base_stat.toString().padStart(3,"0");
+  atkValue.innerText = data.stats[1].base_stat.toString().padStart(3,"0");
+  defValue.innerText = data.stats[2].base_stat.toString().padStart(3,"0");
+  satkValue.innerText = data.stats[3].base_stat.toString().padStart(3,"0");
+  sdefValue.innerText = data.stats[4].base_stat.toString().padStart(3,"0");
+  spdValue.innerText = data.stats[5].base_stat.toString().padStart(3,"0");
+  progressBar.forEach((el,i)=>{
+    el.setAttribute('style', `--color-bar: ${generateTypeColor(data.types[0].type.name)};
+      `);
+    document.documentElement.style.setProperty("--color-bg",generateTypeColor(data.types[0].type.name)+"8a");
+
+    // console.log(generateTypeColor(data.types[0].type.name)+"8a");
+    el.value = data.stats[i].base_stat;
+  })
 }
+
+
+document.querySelector('.back').addEventListener('click', ()=> window.location.href = `index.html`);
 
 getPokemonData(pokemonId);
